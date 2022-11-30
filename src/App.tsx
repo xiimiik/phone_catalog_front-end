a56d80f945b802100424f69c6be39da967cd5440
Artur Nagaycev <artur.nagaycev@gmail.com>
2022-11-30 21:21:52 +0200

    implemented routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
+import { Link, Route, Routes } from "react-router-dom";
 import { Grid } from "./components/Grid/Grid";
 import { GridItem } from "./components/GridItem/GridItem";
 import { ProductCard } from "./components/ProductCard";
 
 function App() {
   return (
-    <Grid>
-      <GridItem from={1} to={7}>
-        <ProductCard />
-      </GridItem>
+    <>
+      <Link to="/">Home</Link>
+      <Link to="/phones">Phones</Link>
 
-      <GridItem from={7} to={13}>
-        <ProductCard />
-      </GridItem>
+      <Routes>
+        <Route path="/" element={<span>123</span>} />
+        <Route path="/phones" element={
+        <>
+          <Grid>
+            <GridItem from={1} to={7}>
+              <ProductCard />
+            </GridItem>
 
-      <GridItem from={13} to={19}>
-        <ProductCard />
-      </GridItem>
+            <GridItem from={7} to={13}>
+              <ProductCard />
+            </GridItem>
 
-      <GridItem from={19} to={25}>
-        <ProductCard />
-      </GridItem>
-    </Grid>
+            <GridItem from={13} to={19}>
+              <ProductCard />
+            </GridItem>
+
+            <GridItem from={19} to={25}>
+              <ProductCard />
+            </GridItem>
+          </Grid>
+        </>}>
+
+        </Route>
+      </Routes>
+    </>
   )
 };
 
