de9fba357a09d2183bc462593c80147cc08848a8
illia-sosnytskiy <kdcompany2018@gmail.com>
2022-12-01 21:43:11 +0200

    rebuild file structure

---
+++ src/App/App.tsx
@@ -0,0 +1,36 @@
+import { Route, Routes } from 'react-router-dom';
+
+import { HomePage } from '../pages/HomePage';
+import { CatalogPage } from '../pages/CatalogPage';
+import { Nav } from '../components/Nav';
+import { FavoritesPage } from '../pages/FavoritesPage';
+import { CartPage } from '../pages/CartPage';
+import { ItemCardPage } from '../pages/ItemCardPage';
+import { NotYet } from '../pages/NotYet';
+import { Footer } from '../components/Footer';
+import { NotFoundPage } from '../pages/NotFoundPage';
+
+import s from './App.module.scss';
+
+export function App() {
+  return (
+    <div className={s.wrapper}>
+      <Nav />
+
+      <main className={s.main}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/phones" element={<CatalogPage />} />
+          <Route path="/tablets" element={<NotYet />} />
+          <Route path="/accessories" element={<NotYet />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/item" element={<ItemCardPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
+  );
+}
