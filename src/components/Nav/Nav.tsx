9147471c143630d449a6af2567e6b40a62bfd123
illia-sosnytskiy <kdcompany2018@gmail.com>
2022-12-01 21:27:07 +0200

    fixed aftrer mentor review

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,75 +1,62 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import logoImg from '../../assets/img/Logo.svg';
 import favouritesImg from '../../assets/img/Favourites.svg';
 import shoppingBagImg from '../../assets/img/ShoppingBag.svg';
 import menuImg from '../../assets/img/Menu.svg';
+
 import s from './Nav.module.scss';
 import cn from 'classnames';
+
+const navLinks = [
+  {to: "/", text: "home"},
+  {to: "/phones", text: "phones"},
+  {to: "/tablets", text: "tablets"},
+  {to: "/accessories", text: "accessories"},
+];
+
+const boxLinks = [
+  {to: "/favorites", imgSrc: favouritesImg, alt: 'favorites'},
+  {to: "/cart", imgSrc: shoppingBagImg, alt: 'cart'},
+];
 
 export const Nav: React.FC = () => {
   return (
     <nav className={s.nav}>
-      <NavLink to="/">
+      <Link to="/">
         <img 
           src={logoImg}
           alt="Logo"
           className={s.nav__logo}
         />
-      </NavLink>
+      </Link>
 
       <ul className={s.nav__list}>
-        <li className={s.nav__item}>
-          <NavLink 
-          to="/" 
-          className={
-            ({ isActive }) => cn(s.nav__link, {[s['nav__link_active']]: isActive })
-          }
-        >
-          home</NavLink>
-        </li>
-        <li className={s.nav__item}>
-          <NavLink 
-          to="/phones" 
-          className={
-            ({ isActive }) => cn(s.nav__link, {[s['nav__link_active']]: isActive })
-          }
-        >
-          phones</NavLink>
-        </li>
-        <li className={s.nav__item}>
-          <NavLink 
-          to="/tablets" 
-          className={
-            ({ isActive }) => cn(s.nav__link, {[s['nav__link_active']]: isActive })
-          }
-        >
-          tablets</NavLink>
-        </li>
-        <li className={s.nav__item}>
-          <NavLink 
-          to="/accessories" 
-          className={
-            ({ isActive }) => cn(s.nav__link, {[s['nav__link_active']]: isActive })
-          }
-        >
-          accessories</NavLink>
-        </li>
+        {navLinks.map(({ to, text }) => (
+          <li key={text} className={s.nav__item}>
+            <NavLink 
+              to={to} 
+              className={
+                ({ isActive }) => cn(s.nav__link, {[s['nav__link_active']]: isActive })
+              }
+            >
+              {text}
+            </NavLink>
+          </li>
+        ))}
       </ul>
 
       <div className={s.nav__cart}>
-        <NavLink to="/favorites" className={s.nav__cart_item}>
-          <img src={favouritesImg} alt="Favourites"/>
-        </NavLink>
-
-        <NavLink to="/cart" className={s.nav__cart_item}>
-          <img src={shoppingBagImg} alt="Shopping Bag" />
-        </NavLink>
+        {boxLinks.map(({ to, imgSrc, alt }) => (
+          <NavLink key={alt} to={to} className={s.nav__cart_item}>
+            <img src={imgSrc} alt={alt}/>
+          </NavLink>
+        ))}
       </div>
 
-      <NavLink to="/menu" className={s.nav__menu}>
+      <Link to="/menu" className={s.nav__menu}>
         <img src={menuImg} alt="Shopping Bag" />
-      </NavLink>
+      </Link>
     </nav>
   );
 }
