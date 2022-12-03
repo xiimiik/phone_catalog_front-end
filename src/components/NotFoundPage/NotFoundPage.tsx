bb23c514e348eafcd1c053fd6336b2eafca4ef8b
Artur Nagaycev <artur.nagaycev@gmail.com>
2022-12-03 23:53:20 +0200

    implemented 404 page

---
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import { Container } from '../Container';
+import s from './NotFoundPage.module.scss';
+import img from '../../assets/img/404_page.png';
+
+export const NotFoundPage = () => (
+  <Container>
+    <div className={s.description}>
+      <p className={s.description__title}>
+        Error 404.
+        <br />
+        Page not found.
+        <br />
+        <br />
+        {'Hey Siri, '}
+        <br />
+        <Link to="/">
+          <span className={s.description__link}>
+            take me home
+          </span>
+        </Link>
+      </p>
+
+      <img
+        className={s.description__image}
+        src={img}
+        alt="iphone 14"
+      />
+    </div>
+  </Container>
+
+);
